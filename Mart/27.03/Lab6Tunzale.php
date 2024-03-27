8996c3ed69907614e9f49234dd1a7612f128a4cc
Ilkin <ilkinh542@gmail.com>
2024-03-27 14:10:49 +0400

    Laboratoriya 6 tamamlandi.

---
+++ Mart/27.03/Lab6Tunzale.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+<body>
+
+<?php
+
+$rows = 9;
+
+$cols = 9;
+
+echo("<table>");
+
+for ($r=1; $r <=$rows; $r++) {
+
+echo("<tr>");
+for ($c=1; $c <=$cols; $c++)
+
+{
+echo('<td>'. "$c * $r = ". ($c*$r). "&emsp;". '</td>');
+
+} echo('</tr>');
+
+}
+
+echo("</table>");
+
+?>
+</body> 
+</html>
